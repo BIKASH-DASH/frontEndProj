f606105adc63efc0f87e420f0db6efd3e4229f5f
bikash <vikashdash004@gmail.com>
2021-08-10 07:33:29 +0530

    Write functional React component

--- hoc_example/src/components/MouseHoverComponent.js
+++ hoc_example/src/components/MouseHoverComponent.js
@@ -2,10 +2,10 @@ import React from 'react';
 import withHoverHOC from './withHoverHOC';
 
 function MouseHoverComponent(props) {
-    const { textColor, textHover, ...divProps } = props;
+    const { textColor, textHover,textContent, ...divProps } = props;
     return (
         <div>
-            <h2 {...divProps} style={{ color: textColor }}>Mouse {textHover ? 'Over' : 'Out'}</h2>
+            <h2 {...divProps} style={{ color: textColor }}>{textContent}</h2>
         </div>
     )
 }
--- hoc_example/src/components/withHoverHOC.js
+++ hoc_example/src/components/withHoverHOC.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 const withHoverHOC = (WrappedComponent) => {
     const WithHover = () => {
         const [textHover, setHover] = useState(false);
         const [textColor, setTextColor] = useState('#888');
+        const [textContent,setTextContent] = useState('Mouse Out');
+        useEffect(() => {
+            const textEffect = (textHover)?"Mouse Over":"Mouse Out";
+            setTextContent(textEffect);
+        }, [textHover])
         return <WrappedComponent
             textColor={textColor}
             textHover={textHover}
+            textContent={textContent}
             onMouseOver={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
             onClick={() => setTextColor('red')} />
